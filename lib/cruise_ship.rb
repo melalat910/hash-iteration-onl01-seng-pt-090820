4c92ec8df7b1f57c52ef12eca1003920640cbcef
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-19 10:05:06 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,4 @@
-def happy_birthday(birthday_kids)
-  # add your code snippet here!	  birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
-  end
-end
+
 # passengers = {
 passengers = {
 suite_a: "Amanda Presley",
@@ -11,6 +7,13 @@ suite_c: "Alfred Tennyson",
 suite_d: "Charlie Chaplin",
 suite_e: "Crumpet the Elf"
 }
+
+def happy_birthday(birthday_kids)
+  # add your code snippet here!	  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
+end
+
 winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")

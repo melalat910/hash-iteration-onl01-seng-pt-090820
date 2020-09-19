04133002e88b04801415561a8bd585e3951b7d24
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-19 10:20:54 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,7 +7,7 @@ birthday_kids = {
 
 
   def happy_birthday(birthday_kids)
-    # add your code snippet here!	 
+    # add your code snippet here!
       puts "Happy Birthday #{name}! You are now #{age} years old!"
     end
 

5064096aad0b3414d5c8ca38170a9fb3d0857e35
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-19 10:02:48 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,3 +6,11 @@ suite_c: "Alfred Tennyson",
 suite_d: "Charlie Chaplin",
 suite_e: "Crumpet the Elf"
 }
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+
+winner
